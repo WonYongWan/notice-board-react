e493241d5e4e639a35000a3d101120601afdf007
WonYongWan <wonxx777@gmail.com>
2026-08-28 12:29:41 +0900

    notice list item 스타일링

--- src/components/notice/NoticeList.jsx
+++ src/components/notice/NoticeList.jsx
@@ -25,11 +25,17 @@ const NoticeList = ({ data, isLoading, currentPage, increaseViews }) => {
             <Link to={`/notices/${noticeItem.id}`} onClick={() => increaseViews(noticeItem.id)}>
               <div className="notice-list__item-box">
                 <div className="notice-list__info">
-                  <p>{noticeItem.id}</p>
-                  <p>{noticeItem.title}</p>
+                  <div className="notice-list__user">
+                    <img className="notice-list__avatar" src="https://img.magnific.com/free-vector/elegant-man-profile-avatar-character_24877-83238.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
+                    <span className="notice-list__nickname">ㅇㅇㅇ</span>
+                    <span className="notice-list__time">date : {noticeItem.updated_at.split('T')[0]}</span>
+                  </div>
+                  <p className="notice-list__title">{noticeItem.title}</p>
                 </div>
                 <div className="notice-list__sub-info">
-                  {noticeItem.views} | {noticeItem.writer} | {noticeItem.updated_at.split('T')[0]}
+                  <span className="notice-list__views">views : {noticeItem.views}</span>
+                  <span className="notice-list__comment">comment : 10</span>
+                  <span className="notice-list__like">like : 10</span>
                 </div>
               </div>
             </Link>
